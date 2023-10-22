b1b21f7ed0543bd181414e01467b4be0e22da619
Javid Ahmed <javidahmed@icloud.com>
2023-10-22 03:07:24 +0100

    Update chatbot message loading and saving and personality configuration

--- chatbot_backend/app.py
+++ chatbot_backend/app.py
@@ -5,19 +5,36 @@ from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 
 from chatbot.chatbot import Chatbot
-from helpers.general_helpers import load_config
+from helpers.general_helpers import system_msg
 
 PERSONALITIES_FOLDER = Path("./personalities")
 CHAT_HISTORY_FOLDER = Path("./history")
 
 app = Flask(__name__)
-chatbot = Chatbot(load_config(PERSONALITIES_FOLDER / "piper.json"), chat_history_directory=CHAT_HISTORY_FOLDER)
+chatbot = Chatbot(
+    personality_config_filepath=PERSONALITIES_FOLDER / "piper.json",
+    chat_history_directory=CHAT_HISTORY_FOLDER,
+)
+
+
+@app.route("/api/chat/load", methods=["GET"])
+def load_chat():
+    if request.method == "GET":
+        chatbot.load_chat()
+        return jsonify(success=True)
+
+
+@app.route("/api/chat/end", methods=["GET"])
+def end_chat():
+    if request.method == "GET":
+        chatbot.end_chat()
+        return jsonify(success=True)
 
 
 @app.route("/api/messages", methods=["GET", "POST"])
 def messages():
     if request.method == "GET":
-        return jsonify({"messages": chatbot.chat_history})
+        return jsonify({"messages": chatbot._messages})
     elif request.method == "POST":
         data = json.loads(request.data)
         message = data["message"]
@@ -26,14 +43,12 @@ def messages():
 
 
 if __name__ == "__main__":
+    system_msg("Loading environment variables...")
     load_dotenv()
 
     try:
-        chatbot.load_chat()
-    except FileNotFoundError:
-        print("Chat history not found!")
-
-    try:
-        app.run()
+        system_msg("Running app...")
+        app.run(host="0.0.0.0")
+        system_msg("App running!...")
     except (KeyboardInterrupt, SystemExit):
-        chatbot.save_chat()
+        system_msg("Shutting down program...")
--- chatbot_backend/chatbot/chatbot.py
+++ chatbot_backend/chatbot/chatbot.py
@@ -3,31 +3,47 @@ import os
 import openai
 from openai.error import RateLimitError
 
-from helpers.general_helpers import read_txt_file, write_to_txt_file
+from helpers.general_helpers import load_json, read_txt_file, update_json_file, write_to_txt_file
 
 
 class Chatbot:
-    def __init__(self, personality_config, chat_history_directory):
-        self.name = personality_config["name"]
-        self.personality = personality_config["personality"]
-        self.rules = personality_config["rules"]
+    def __init__(self, personality_config_filepath, chat_history_directory):
+        self._api_key = os.getenv("API_KEY")
+        openai.api_key = self._api_key
 
-        self.api_key = os.getenv("API_KEY")
-        openai.api_key = self.api_key
+        self._personality_config_filepath = personality_config_filepath
+        self._personality_config = None
+        self.set_personality()
 
-        self.model = personality_config["model"]
-        self.temperature = personality_config["temperature"]
-        self.max_tokens = personality_config["max_tokens"]
-        self.n = personality_config["n"]
-        self.stop = personality_config["stop"]
-        self.max_messages = personality_config["max_messages"]
-
-        self.chat_history_directory = chat_history_directory
+        self._chat_history_directory = chat_history_directory
+        self._chat_history_filename = self._chat_history_directory / f"{self.name}.txt"
         self._messages = []
-        self._msg_system(
-            f"Your name is {self.name}. {self.personality}. You follow these rules: \n{Chatbot.create_numbered_list(self.rules)}"
-        )
-        self.num_messages_at_start = 1
+        self._num_messages_at_start = 1
+
+    @property
+    def personality_config(self):
+        if not self._personality_config:
+            self._personality_config = load_json(self._personality_config_filepath)
+
+        return self._personality_config
+
+    @personality_config.setter
+    def personality_config(self, new_config):
+        self._personality_config = new_config
+        update_json_file(self._personality_config_filepath, new_config)
+        self.set_personality()
+
+    def set_personality(self):
+        self.name = self.personality_config["name"]
+        self.personality = self.personality_config["personality"]
+        self.rules = self.personality_config["rules"]
+
+        self.model = self.personality_config["model"]
+        self.temperature = self.personality_config["temperature"]
+        self.max_tokens = self.personality_config["max_tokens"]
+        self.n = self.personality_config["n"]
+        self.stop = self.personality_config["stop"]
+        self.max_messages = self.personality_config["max_messages"]
 
     @staticmethod
     def create_numbered_list(lst):
@@ -37,7 +53,7 @@ class Chatbot:
 
     def _add_msg(self, role, msg):
         if len(self._messages) >= self.max_messages:
-            del self._messages[self.num_messages_at_start : self.num_messages_at_start + 2]
+            del self._messages[self._num_messages_at_start : self._num_messages_at_start + 2]
 
         self._messages.append({"role": role, "content": msg})
 
@@ -73,53 +89,31 @@ class Chatbot:
         finally:
             return reply
 
-    @property
-    def chat_history(self):
-        return self._messages
-
-    @property
-    def chat_history_filename(self):
-        return self.chat_history_directory / f"{self.name}.txt"
-
     def load_chat(self):
-        if os.path.exists(self.chat_history_filename):
-            self._messages += eval(read_txt_file(self.chat_history_filename))[1:]
-        else:
-            raise FileNotFoundError("Chat history does not exist.")
+        try:
+            if not os.path.exists(self._chat_history_filename):
+                raise FileNotFoundError("Chat history does not exist.")
 
-        self.num_messages_at_start = len(self._messages)
+            self._messages = eval(read_txt_file(self._chat_history_filename))
+        except (FileNotFoundError, SyntaxError):
+            self._msg_system(
+                f"Your name is {self.name}. {self.personality}. You follow these rules: \n{Chatbot.create_numbered_list(self.rules)}"
+            )
 
-    def save_chat(self):
+        self._num_messages_at_start = len(self._messages)
+
+    def summarise_chat(self, num_messages):
+        self._msg_user(f"Can you summarise the last {num_messages} messages? Keep your response concise.")
         summary = self._generate_response()
-        del self._messages[self.num_messages_at_start :]
-        self._msg_system(summary)
-        write_to_txt_file(self.chat_history_filename, str(self._messages))
+        del self._messages[self._num_messages_at_start :]
+        self._msg_system(f"Summary of last conversation: {summary}")
 
-    def start_chat(self, load_chat=False):
-        response = ""
+    def end_chat(self):
+        num_messages = len(self._messages) - self._num_messages_at_start
 
-        if load_chat:
-            try:
-                self.load_chat()
-            except FileNotFoundError:
-                response += "Error loading chat history. Starting from scratch... "
-
-        self._msg_user(f"Hi, {self.name}!")
-
-        response += self._generate_response()
-        return response
-
-    def end_chat(self, save_chat=False):
-        self._msg_user(f"Goodbye, {self.name}!")
-        response = self._generate_response()
-
-        num_messages = len(self._messages) - self.num_messages_at_start
-
-        if save_chat and num_messages > 4:
-            self._msg_user(f"Can you summarise the last {num_messages} messages? Keep your response concise.")
-            self.save_chat()
-
-        return response
+        if num_messages > 0:
+            self.summarise_chat(num_messages)
+            write_to_txt_file(self._chat_history_filename, str(self._messages))
 
     def chat(self, msg):
         if len(msg) == 0:
